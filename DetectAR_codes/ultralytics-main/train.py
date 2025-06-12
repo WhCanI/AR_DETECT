85c55b620533ca7ddcb46f493b3e74b5c6bacea4
WhCanI <167731391+WhCanI@users.noreply.github.com>
2025-06-12 11:47:04 +0800

    train.py

--- DetectAR_codes/ultralytics-main/train.py
+++ DetectAR_codes/ultralytics-main/train.py
@@ -2,9 +2,5 @@ from ultralytics import YOLO
 
 # Load a model
 model = YOLO("DetectAR/DetectAR_codes/models/yolov8-ecasdcn.yaml").load("./yolov8s.pt")  # build a new model from scratch
-
-# Use the model
-model.train(data="/DetectAR/DetectAR_data/AR.yaml", epochs=100, batch=16, imgsz=1024, patience=50, single_cls=True, optimizer='AdamW', lr0=0.001, iou=0.5, resume=True)  # train the model
-
-# Validate the model
-metrics = model.val()# validate model performance on the validation set
+model.train(data="/DetectAR/DetectAR_data/AR.yaml", epochs=100, batch=16, imgsz=1024, patience=50, single_cls=True, optimizer='AdamW', lr0=0.001, resume=True)  # train the model
+metrics = model.val()
